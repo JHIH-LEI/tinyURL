ee4baa5188ab5bb57521f6231828700cba572f7a
JHIH-LEI <vicky88888@gmail.com>
2021-07-28 04:48:40 +0800

    feat: add express-handlebars

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 
 const PORT = 3000
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 app.listen(PORT, () => console.log(`app is on http://${PORT}`))
