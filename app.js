341d11d94ec8470cf31a7206aee4c4b3ec1c8049
JHIH-LEI <vicky88888@gmail.com>
2021-07-28 21:16:08 +0800

    feat: add index page

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 
+require('./config/mongoose')
+
 const PORT = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
 
 app.listen(PORT, () => console.log(`app is on http://${PORT}`))
