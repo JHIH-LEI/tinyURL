3782c5b1901cdb21e929490fde61546409c5fbae
JHIH-LEI <vicky88888@gmail.com>
2021-07-29 02:31:49 +0800

    feat: add store url data function & modify url models

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 
 require('./config/mongoose')
+const Url = require('./models/url')
 const { getNumAndChar, getRandomPath } = require('./tools/helper')
 
 const PORT = 3000
@@ -19,17 +20,25 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   // 獲取表單傳來的網址
   const { url } = req.body
-  res.send(url)
   // 產生隨機路由
   let list = getNumAndChar() //0-9 A-Z a-z
   let pathname = getRandomPath(5, list)
   // 檢查是否存在，沒有的話存進去
-
+  async function storeUrl() {
+    let match = await Url.find({ tiny: pathname }).lean() //如果pathname已存在，回傳已存在結果
+    // 如果已存在，對象會被存到match陣列中，長度不為0，為真，進行重新取得縮網址
+    while (match.length) {
+      pathname = getRandomPath(5, list)
+      match = await Url.find({ tiny: pathname }).lean()
+    }
+    // 取到獨一無二的縮網址，之後存到資料庫當中
+    await Url.create({ url: url, tiny: pathname })
+      .then(() => res.send(url))
+  }
+  storeUrl()
 })
 
 // 處理縮網址請求
 // req 縮網址，回應對照原網址
-
-
 app.listen(PORT, () => console.log(`app is on http://${PORT}`))
 
--- models/url.js
+++ models/url.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const urlSchema = new Schema({
-  path: {
+  url: {
     type: String,
     required: true
   },
