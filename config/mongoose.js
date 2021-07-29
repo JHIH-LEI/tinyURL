340726b6545dc3f8ffe7aba848c33f97e9d36754
JHIH-LEI <vicky88888@gmail.com>
2021-07-29 18:04:46 +0800

    feat: modify mongodb URI

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const MONGODB_URI = process.env.MOGODB_URI || 'mongodb://localhost:27017/tinyurl'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/tinyurl'
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
 
